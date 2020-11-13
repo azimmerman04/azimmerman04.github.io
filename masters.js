1f8a7ae74597d71b2c19bc014162233953e4ddf4
Adam Zimmerman <adam.zimmerman@rovisys.com>
2020-11-13 16:50:21 -0600

    adding auto-update

--- masters.js
+++ masters.js
@@ -1,40 +1,43 @@
 angular.module('mastersApp', [])
-  .controller('MastersController', ['$scope','$http', function($scope, $http) {
+  .controller('MastersController', ['$scope','$http', '$interval', function($scope, $http, $interval) {
     var masters = this;
 
-    masters.teams = [
-      { owner: "Adam",
-        players: ["Bryson DeChambeau","Xander Schauffele","Patrick Reed","Jason Day","Matthew Wolff","Justin Rose","Cameron Champ","Abraham Ancer"],
-        scoresByRound: [[],[],[],[]],
-        playerTotals: []
-      },
-      { owner: "Danny",
-        players: ["Jon Rahm","Rory McIlroy","Bubba Watson","Patrick Cantlay","Tiger Woods","Jordan Spieth","Tyrrell Hatton","Matt Kuchar"],
-        scoresByRound: [[],[],[],[]],
-        playerTotals: []
-      },
-      { owner: "Drew",
-        players: ["Dustin Johnson","Brooks Koepka","Hideki Matsuyama","Adam Scott","Rickie Fowler","Kevin Kisner","Paul Casey","Shane Lowry"],
-        scoresByRound: [[],[],[],[]],
-        playerTotals: []
-      },
-      { owner: "Ryan",
-        players: ["Justin Thomas","Webb Simpson","Collin Morikawa","Tony Finau","Tommy Fleetwood","Louis Oosthuizen","Sungjae Im","Matthew Fitzpatrick"],
-        scoresByRound: [[],[],[],[]],
-        playerTotals: []
-      }
-    ];
+    masters.init = function(){
+      masters.updateTime = "";
+      masters.teams = [
+        { owner: "Adam",
+          players: ["Bryson DeChambeau","Xander Schauffele","Patrick Reed","Jason Day","Matthew Wolff","Justin Rose","Cameron Champ","Abraham Ancer"],
+          scoresByRound: [[],[],[],[]],
+          playerTotals: []
+        },
+        { owner: "Danny",
+          players: ["Jon Rahm","Rory McIlroy","Bubba Watson","Patrick Cantlay","Tiger Woods","Jordan Spieth","Tyrrell Hatton","Matt Kuchar"],
+          scoresByRound: [[],[],[],[]],
+          playerTotals: []
+        },
+        { owner: "Drew",
+          players: ["Dustin Johnson","Brooks Koepka","Hideki Matsuyama","Adam Scott","Rickie Fowler","Kevin Kisner","Paul Casey","Shane Lowry"],
+          scoresByRound: [[],[],[],[]],
+          playerTotals: []
+        },
+        { owner: "Ryan",
+          players: ["Justin Thomas","Webb Simpson","Collin Morikawa","Tony Finau","Tommy Fleetwood","Louis Oosthuizen","Sungjae Im","Matthew Fitzpatrick"],
+          scoresByRound: [[],[],[],[]],
+          playerTotals: []
+        }
+      ];
 
-    masters.pools = {
-      round1LowMan: [],
-      round1LowFoursome: [],
-      round2LowMan: [],
-      round2LowFoursome: [],
-      round3LowMan: [],
-      round3LowFoursome: [],
-      round4LowMan: [],
-      round4LowFoursome: [],
-      LowTournamentFoursome: []
+      masters.pools = {
+        round1LowMan: [],
+        round1LowFoursome: [],
+        round2LowMan: [],
+        round2LowFoursome: [],
+        round3LowMan: [],
+        round3LowFoursome: [],
+        round4LowMan: [],
+        round4LowFoursome: [],
+        LowTournamentFoursome: []
+      };
     };
 
     function calculateScore(pars,roundData) {
@@ -76,55 +79,62 @@ angular.module('mastersApp', [])
       return players;
     }
 
-      $http.get("https://www.masters.com/en_US/scores/feeds/scores.json")
-      .then(function(response){
-        var mastersData = response.data.data;
-        for (var i = 0; i < masters.teams.length; i++){
-          var team = masters.teams[i];
-          for (var j = 0; j < team.players.length;j++){
-            var playerName = team.players[j];
-            for (var k = 0; k < mastersData.player.length; k++){
-              var playerData = mastersData.player[k];
-              if ((playerData.first_name + " " + playerData.last_name) === playerName){
-                var scores = [calculateScore(mastersData.pars.round1,playerData.round1.scores),
-                  calculateScore(mastersData.pars.round1,playerData.round2.scores),
-                  calculateScore(mastersData.pars.round1,playerData.round3.scores),
-                  calculateScore(mastersData.pars.round1,playerData.round4.scores)];
-                  var playerTotal = 0;
-                  for (var l = 0; l < 4; l++){
-                    var roundScore = scores[l];
-                    playerTotal = playerTotal + roundScore;
-                    team.scoresByRound[l].push(roundScore);
-                  }
-                  team.playerTotals.push({player: playerName, score: playerTotal});
-                break;
-              } else if (k === mastersData.player.length){
-                console.log("Player not found: " + playerName);
+    masters.calculate = function() {
+        $http.get("https://www.masters.com/en_US/scores/feeds/scores.json")
+        .then(function(response){
+          masters.init();
+          var mastersData = response.data.data;
+          for (var i = 0; i < masters.teams.length; i++){
+            var team = masters.teams[i];
+            for (var j = 0; j < team.players.length;j++){
+              var playerName = team.players[j];
+              for (var k = 0; k < mastersData.player.length; k++){
+                var playerData = mastersData.player[k];
+                if ((playerData.first_name + " " + playerData.last_name) === playerName){
+                  var scores = [calculateScore(mastersData.pars.round1,playerData.round1.scores),
+                    calculateScore(mastersData.pars.round1,playerData.round2.scores),
+                    calculateScore(mastersData.pars.round1,playerData.round3.scores),
+                    calculateScore(mastersData.pars.round1,playerData.round4.scores)];
+                    var playerTotal = 0;
+                    for (var l = 0; l < 4; l++){
+                      var roundScore = scores[l];
+                      playerTotal = playerTotal + roundScore;
+                      team.scoresByRound[l].push(roundScore);
+                    }
+                    team.playerTotals.push({player: playerName, score: playerTotal});
+                  break;
+                } else if (k === mastersData.player.length){
+                  console.log("Player not found: " + playerName);
+                }
               }
             }
           }
-        }
-        
-        for (var i = 0; i < masters.teams.length; i++){
-          var team = masters.teams[i];
-          for (var j = 0; j < 4; j++){
-            var scoresForRound = team.scoresByRound[j].slice();
-            var scoresForRoundSorted = scoresForRound.sort(function(a,b){return a - b});
-            masters.pools["round" + (j+1) + "LowMan"].push({team: team.owner, score: scoresForRoundSorted[0], player: findPlayerWithScore(team, j, scoresForRoundSorted[0])});
-            var foursomePlayers = getFoursomeForScores(team, j, [scoresForRoundSorted[0],scoresForRoundSorted[1],scoresForRoundSorted[2],scoresForRoundSorted[3]]);
-            masters.pools["round" + (j+1) + "LowFoursome"].push({team: team.owner, score: scoresForRoundSorted[0] + scoresForRoundSorted[1] + scoresForRoundSorted[2] + scoresForRoundSorted[3], players: foursomePlayers});
+          
+          for (var i = 0; i < masters.teams.length; i++){
+            var team = masters.teams[i];
+            for (var j = 0; j < 4; j++){
+              var scoresForRound = team.scoresByRound[j].slice();
+              var scoresForRoundSorted = scoresForRound.sort(function(a,b){return a - b});
+              masters.pools["round" + (j+1) + "LowMan"].push({team: team.owner, score: scoresForRoundSorted[0], player: findPlayerWithScore(team, j, scoresForRoundSorted[0])});
+              var foursomePlayers = getFoursomeForScores(team, j, [scoresForRoundSorted[0],scoresForRoundSorted[1],scoresForRoundSorted[2],scoresForRoundSorted[3]]);
+              masters.pools["round" + (j+1) + "LowFoursome"].push({team: team.owner, score: scoresForRoundSorted[0] + scoresForRoundSorted[1] + scoresForRoundSorted[2] + scoresForRoundSorted[3], players: foursomePlayers});
+            }
+            var playerTotalsSorted = team.playerTotals.sort(function(a,b){return a.score - b.score});
+            var playersTotalPlayers = [playerTotalsSorted[0].player + " " + playerTotalsSorted[0].score.toString(),playerTotalsSorted[1].player + " " + playerTotalsSorted[1].score.toString(), playerTotalsSorted[2].player + " " + playerTotalsSorted[2].score.toString(), playerTotalsSorted[3].player + " " + playerTotalsSorted[3].score.toString()];
+            masters.pools.LowTournamentFoursome.push({team: team.owner, score: playerTotalsSorted[0].score + playerTotalsSorted[1].score + playerTotalsSorted[2].score + playerTotalsSorted[3].score,players: playersTotalPlayers});
           }
-          var playerTotalsSorted = team.playerTotals.sort(function(a,b){return a.score - b.score});
-          var playersTotalPlayers = [playerTotalsSorted[0].player + " " + playerTotalsSorted[0].score.toString(),playerTotalsSorted[1].player + " " + playerTotalsSorted[1].score.toString(), playerTotalsSorted[2].player + " " + playerTotalsSorted[2].score.toString(), playerTotalsSorted[3].player + " " + playerTotalsSorted[3].score.toString()];
-          masters.pools.LowTournamentFoursome.push({team: team.owner, score: playerTotalsSorted[0].score + playerTotalsSorted[1].score + playerTotalsSorted[2].score + playerTotalsSorted[3].score,players: playersTotalPlayers});
-        }
 
-        for (var i = 0; i < 4; i++){
-          masters.pools["round" + (i+1) + "LowMan"].sort(function(a,b){return a.score - b.score});
-          masters.pools["round" + (i+1) + "LowFoursome"].sort(function(a,b){return a.score - b.score});
-        }
+          for (var i = 0; i < 4; i++){
+            masters.pools["round" + (i+1) + "LowMan"].sort(function(a,b){return a.score - b.score});
+            masters.pools["round" + (i+1) + "LowFoursome"].sort(function(a,b){return a.score - b.score});
+          }
 
-        masters.pools.LowTournamentFoursome.sort(function(a,b){return a.score - b.score});
-      });
+          masters.pools.LowTournamentFoursome.sort(function(a,b){return a.score - b.score});
+          masters.updateTime = (new moment()).format('MMM DD, YYYY hh:mm:ss A');
+        });
+      };
+      masters.init();
+      masters.calculate();
+      $interval(masters.calculate,30000);
     }
   ]);
