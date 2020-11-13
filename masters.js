e438a809c6cacbc0feb8becca75a40592246337a
Adam Zimmerman <adam.zimmerman@rovisys.com>
2020-11-12 18:42:57 -0600

    initial commit

---
+++ masters.js
@@ -0,0 +1,100 @@
+angular.module('mastersApp', [])
+  .controller('MastersController', ['$scope','$http', function($scope, $http) {
+    var masters = this;
+
+    masters.teams = [
+      { owner: "Adam",
+        players: ["Bryson DeChambeau","Xander Schauffele","Patrick Reed","Jason Day","Matthew Wolff","Justin Rose","Cameron Champ","Abraham Ancer"],
+        scoresByRound: [[],[],[],[]],
+        playerTotals: []
+      },
+      { owner: "Danny",
+        players: ["Jon Rahm","Rory McIlroy","Bubba Watson","Patrick Cantlay","Tiger Woods","Jordan Spieth","Tyrrell Hatton","Matt Kuchar"],
+        scoresByRound: [[],[],[],[]],
+        playerTotals: []
+      },
+      { owner: "Drew",
+        players: ["Dustin Johnson","Brooks Koepka","Hideki Matsuyama","Adam Scott","Rickie Fowler","Kevin Kisner","Paul Casey","Shane Lowry"],
+        scoresByRound: [[],[],[],[]],
+        playerTotals: []
+      },
+      { owner: "Ryan",
+        players: ["Justin Thomas","Webb Simpson","Collin Morikawa","Tony Finau","Tommy Fleetwood","Louis Oosthuizen","Sungjae Im","Matthew Fitzpatrick"],
+        scoresByRound: [[],[],[],[]],
+        playerTotals: []
+      }
+    ];
+
+    masters.pools = {
+      round1LowMan: [],
+      round1LowFoursome: [],
+      round2LowMan: [],
+      round2LowFoursome: [],
+      round3LowMan: [],
+      round3LowFoursome: [],
+      round4LowMan: [],
+      round4LowFoursome: [],
+      LowTournamentFoursome: []
+    };
+
+    function calculateScore(pars,roundData) {
+      var score = 0;
+      for (var i = 0; i < 18; i++){
+        if (roundData[i] != null){
+          score = score + (roundData[i] - pars[i]);
+        }
+      }
+      return score;
+    }
+
+      $http.get("https://www.masters.com/en_US/scores/feeds/scores.json")
+      .then(function(response){
+        var mastersData = response.data.data;
+        for (var i = 0; i < masters.teams.length; i++){
+          var team = masters.teams[i];
+          for (var j = 0; j < team.players.length;j++){
+            var playerName = team.players[j];
+            for (var k = 0; k < mastersData.player.length; k++){
+              var playerData = mastersData.player[k];
+              if ((playerData.first_name + " " + playerData.last_name) === playerName){
+                var scores = [calculateScore(mastersData.pars.round1,playerData.round1.scores),
+                  calculateScore(mastersData.pars.round1,playerData.round2.scores),
+                  calculateScore(mastersData.pars.round1,playerData.round3.scores),
+                  calculateScore(mastersData.pars.round1,playerData.round4.scores)];
+                  var playerTotal = 0;
+                  for (var l = 0; l < 4; l++){
+                    var roundScore = scores[l];
+                    playerTotal = playerTotal + roundScore;
+                    if (roundScore != 0){
+                      team.scoresByRound[l].push(roundScore);
+                    }
+                  }
+                  team.playerTotals.push(playerTotal);
+                break;
+              } else if (k === mastersData.player.length){
+                console.log("Player not found: " + playerName);
+              }
+            }
+          }
+        }
+        
+        for (var i = 0; i < masters.teams.length; i++){
+          var team = masters.teams[i];
+          for (var j = 0; j < 4; j++){
+            var scoresForRound = team.scoresByRound[j].slice();
+            var scoresForRoundSorted = scoresForRound.sort(function(a,b){return a - b});
+            masters.pools["round" + (j+1) + "LowMan"].push({team: team.owner, score: scoresForRoundSorted[0]});
+            masters.pools["round" + (j+1) + "LowFoursome"].push({team: team.owner, score: scoresForRoundSorted[0] + scoresForRoundSorted[1] + scoresForRoundSorted[2] + scoresForRoundSorted[3]});
+          }
+          var playerTotalsSorted = team.playerTotals.sort(function(a,b){return a - b});
+          masters.pools.LowTournamentFoursome.push({team: team.owner, score: playerTotalsSorted[0] + playerTotalsSorted[1] + playerTotalsSorted[2] + playerTotalsSorted[3]});
+        }
+
+        for (var i = 0; i < 4; i++){
+          masters.pools["round" + (i+1) + "LowMan"].sort(function(a,b){return a.score - b.score});
+          masters.pools["round" + (i+1) + "LowFoursome"].sort(function(a,b){return a.score - b.score});
+        }
+        masters.pools.LowTournamentFoursome.sort(function(a,b){return a.score - b.score});
+      });
+    }
+  ]);
